51e85041c3ec30f7f0b63d06644996b366ebd3c1
unfinishedd <sam0000@live.nl>
2021-09-17 21:12:24 +0200

    project

---
+++ project1/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use \App\Models\User;
+use \App\Models\Post;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::factory()->create([
+            'name' => 'Sam'
+        ]);
+    
+        Post::factory(5)->create([
+            'user_id' => $user->id
+        ]);
+
+    }
+}
---
+++ project1/resources/views/posts.blade.php
@@ -0,0 +1,25 @@
+@extends ('layout')
+
+@section('banner')
+    <h1>Mooie Header van layout<h1>
+@endsection
+
+@section('content')
+    @foreach ($posts as $post)
+        <article class="{{$loop-> even ? 'foobar' : ''}}">
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+                    {!! $post->title !!}
+                </a>
+            </h1>
+
+            <p>
+                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in<a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a>
+            </p>
+            
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
+@endsection
